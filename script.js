4756a7954ce3ee48a7e3475dc5e9112be943be25
Oliwier <oliwier@klefas.com>
2023-10-01 21:37:52 +0200

    JavaScript (v.1.3)

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const questions = [
         answers: [
             { text: "Asia", correct: false},
             { text: "Australia", correct: true},
-            { text: "arctic", correct: false},
+            { text: "Arctic", correct: false},
             { text: "Africa", correct: false},
         ]
     }
@@ -81,6 +81,7 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }
     else{
         selectedBtn.classList.add("incorrect");
@@ -93,5 +94,31 @@ function selectAnswer(e){
     });
     nextButton.style.display = "block";
 }
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+});
 
 startQuiz();

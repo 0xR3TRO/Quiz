3aad0aa1d2fbe0046809b8486e190c0ce8ec7547
Oliwier <oliwier@klefas.com>
2023-10-01 21:28:52 +0200

    JavaScript (v.1.2)

--- script.js
+++ script.js
@@ -85,6 +85,13 @@ function selectAnswer(e){
     else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
 startQuiz();
